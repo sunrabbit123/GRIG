ce2e153481b40ea2a57a0807cf10abc0054a0663
sunrabbit123 <qudwls185@naver.com>
2021-09-18 23:35:41 +0900

    add : 사진이 없을 시 사진 추가 및 css 처리

--- src/components/UI/atoms/RankingProfile.tsx
+++ src/components/UI/atoms/RankingProfile.tsx
@@ -1,15 +1,20 @@
 import styled from "styled-components";
 
+const RankingProfileContainer = styled.td`
+  border-bottom: 0.1rem solid #e1e1e1;
+  padding: 1.2rem 1.5rem 1.2rem 1rem;
+`;
 const RankingProfileTag = styled.img`
-  border-radius: 30%;
-  border: 1px solid gray;
+  border-radius: 100%;
+  height : 100px;
+  width : 100px;
 `;
 
 const RankingProfile: React.FC<{ link: string }> = (props) => {
   return (
-    <td>
-      <RankingProfileTag src={props.link} />
-    </td>
+    <RankingProfileContainer>
+      <RankingProfileTag src={props.link ?? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"} />
+    </RankingProfileContainer>
   );
 };
 
