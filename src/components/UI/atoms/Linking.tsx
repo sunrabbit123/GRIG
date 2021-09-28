2e53e053b80d96a57f62628b8f7fe3ec213d613f
sunrabbit123 <qudwls185@naver.com>
2021-09-28 16:56:51 +0900

    add : change a tag color

--- src/components/UI/atoms/Linking.tsx
+++ src/components/UI/atoms/Linking.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const Description = styled.a`
+  color: #db7770;
 `;
 
-const HeaderLink: React.FC<{text : string, link : string}> = (props) => {
+const HeaderLink: React.FC<{ text: string; link: string }> = (props) => {
   return (
     <>
       <Description href={props.link}>{props.text}</Description>
