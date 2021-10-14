fe27275d0ec04f6cce01548a53f15eb2e24528e0
sunrabbit123 <qudwls185@naver.com>
2021-10-14 20:43:28 +0900

    hotfix : 중간에 이상한값들어가서 제대로 요청이 되지않던 점 수정

--- src/config/ranking.ts
+++ src/config/ranking.ts
@@ -1,21 +1,21 @@
 import { GraphQLVariables } from "../type/user";
 
-export const getRankingApiUrl: string =
-  "https://d6ui2fy5uj.execute-api.ap-northeast-2.amazonaws.com/api/graphql";
+export const getRankingApiUrl: string = "http://localhost:3000/api/graphql";
+//"https://d6ui2fy5uj.execute-api.ap-northeast-2.amazonaws.com/api/graphql";
 
 export const getGraphQLRankingQuery: Function = (criteria: string) => `
-query getUserRankingQuery($rankingCriteria: String, $rankingCount: Int, $rankingPage: Int, $generation : Int) {
-  ranking(criteria: $rankingCriteria, count: $rankingCount, page: $rankingPage, generation : $generation) {
-    name
-    generation
-    nickname
-    bio
-    public_repos
-    avatar_url
-
-    ${criteria}
+query getUserRankingQuery($rankingCriteria: String, $rankingPage: Int, $rankingCount: Int, $generation: Int) {
+  ranking(criteria: $rankingCriteria, page: $rankingPage, count: $rankingCount, generation : $generation){
+      name
+      generation
+      nickname
+      bio
+      public_repos
+      avatar_url
+      ${criteria}
   }
 }
+
 `;
 
 export const getUserInformCriteria: Function = (
