3371cb515e47cd3f01557510b83e31b5a041baf1
sunrabbit123 <qudwls185@naver.com>
2021-10-14 19:44:04 +0900

    update : ranking query

--- src/config/ranking.ts
+++ src/config/ranking.ts
@@ -4,8 +4,8 @@ export const getRankingApiUrl: string =
   "https://d6ui2fy5uj.execute-api.ap-northeast-2.amazonaws.com/api/graphql";
 
 export const getGraphQLRankingQuery: Function = (criteria: string) => `
-query getUserRankingQuery($rankingCriteria: String, $rankingCount: Int, $rankingPage: Int) {
-  ranking(criteria: $rankingCriteria, count: $rankingCount, page: $rankingPage) {
+query getUserRankingQuery($rankingCriteria: String, $rankingCount: Int, $rankingPage: Int, $generation : Int) {
+  ranking(criteria: $rankingCriteria, count: $rankingCount, page: $rankingPage, generation : $generation) {
     name
     generation
     nickname
@@ -25,5 +25,6 @@ export const getUserInformCriteria: Function = (
     rankingCriteria: criteria,
     rankingCount: 100,
     rankingPage: 1,
+    generation: 0,
   };
 };
